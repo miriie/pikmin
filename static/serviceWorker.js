ddeafaa99febf4efd7d554a2caaf63e8b0678946
Flambedrice <shreya.reddy036@gmail.com>
2024-12-14 14:09:42 +1100

    final touches (flame part)

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -9,18 +9,10 @@ const putInCache = async (request, response) => {
 };
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-  // First try to get the resource from the cache
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
     return responseFromCache;
   }
-
-  // Next try to use the preloaded response, if it's there
-  // NOTE: Chrome throws errors regarding preloadResponse, see:
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
-  // https://github.com/mdn/dom-examples/issues/145
-  // To avoid those errors, remove or comment out this block of preloadResponse
-  // code along with enableNavigationPreload() and the "activate" listener.
   const preloadResponse = await preloadResponsePromise;
   if (preloadResponse) {
     console.info('using preload response', preloadResponse);
@@ -28,12 +20,8 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     return preloadResponse;
   }
 
-  // Next try to get the resource from the network
   try {
     const responseFromNetwork = await fetch(request.clone());
-    // response may be used only once
-    // we need to save clone to put one copy in cache
-    // and serve second one
     putInCache(request, responseFromNetwork.clone());
     return responseFromNetwork;
   } catch (error) {
@@ -41,9 +29,6 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     if (fallbackResponse) {
       return fallbackResponse;
     }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
     return new Response('Network error happened', {
       status: 408,
       headers: { 'Content-Type': 'text/plain' },
@@ -53,7 +38,6 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
 
 const enableNavigationPreload = async () => {
   if (self.registration.navigationPreload) {
-    // Enable navigation preloads!
     await self.registration.navigationPreload.enable();
   }
 };
@@ -67,7 +51,6 @@ self.addEventListener('fetch', (event) => {
     cacheFirst({
       request: event.request,
       preloadResponsePromise: event.preloadResponse,
-      fallbackUrl: './gallery/myLittleVader.jpg',
     })
   );
 });
